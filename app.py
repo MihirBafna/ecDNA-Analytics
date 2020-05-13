ec88d0444f9af8a59a4c3fbf0501d0d881261eda
Mihir Bafna <mihir.bafna@gmail.com>
2020-05-13 15:06:15 -0700

    basic Flask App

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/input')
+def input():
+    return render_template('input.html')
+
+@app.route('/visualize')
+def visualize():
+    return render_template('visualize.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
