7dde92935b488b0684c7f2f48d31bad09d41e49f
Mihir Bafna <mihir.bafna@gmail.com>
2020-05-29 17:57:09 -0700

    converting images to png

---
+++ convertImages.py
@@ -0,0 +1,15 @@
+from PIL import Image
+import os
+
+
+for file in os.listdir("static/img/ecSegOutput/dapi2"):
+    if file.endswith(".tif"):
+        im = Image.open("static/img/ecSegOutput/dapi2/"+file)
+        newim = file[:-3]+"png"
+        im.save("static/img/ecSegOutput/dapi2/"+newim)
+
+for file in os.listdir("static/img/ecSegOutput/orig"):
+    if file.endswith(".tif"):
+        im = Image.open("static/img/ecSegOutput/orig/"+file)
+        newim = file[:-3]+"png"
+        im.save("static/img/ecSegOutput/orig/"+newim)
