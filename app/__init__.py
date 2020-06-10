234b97341326efbc74795124825f0e1a55fedb8e
Mihir Bafna <mihir.bafna@gmail.com>
2020-06-09 19:06:48 -0700

    restructured entire application

---
+++ app/__init__.py
@@ -0,0 +1,8 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,26 +1,24 @@
-from flask import Flask, render_template, redirect, request, session
+from app import app
+from flask import render_template, redirect, request, session
 import os
 from werkzeug.utils import secure_filename
 from datetime import datetime
 
-
-app = Flask(__name__)
-
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
-
-
 @app.route('/')
 def home():
     return render_template('index.html')
+
 
 @app.route('/input')
 def input():
     return render_template('input.html')
 
 
-app.config["IMAGE_UPLOADS"] = "static/img"
+app.config["IMAGE_UPLOADS"] = "app/static/img"
 app.config["ALLOWED_INPUT_IMAGE_EXTENSIONS"] = ["TIF", "TIFF"]
-app.config["ALLOWED_OUTPUT_IMAGE_EXTENSIONS"] = ["JPEG", "JPG", "PNG", "TIF", "TIFF"]
+app.config["ALLOWED_OUTPUT_IMAGE_EXTENSIONS"] = [
+    "JPEG", "JPG", "PNG", "TIF", "TIFF"]
+
 
 def imglist(filepath):  # get image data after ecSeg is run
     print(os.listdir(filepath))
@@ -30,24 +28,27 @@ def imglist(filepath):  # get image data after ecSeg is run
             imagelist.append(file)
     return(imagelist)
 
+
 def allowed_image(filename, inout):
     if not "." in filename:
         return False
     ext = filename.rsplit(".", 1)[1]
     if inout and ext.upper() in app.config["ALLOWED_INPUT_IMAGE_EXTENSIONS"]:
-            return True
+        return True
     elif not inout and ext.upper() in app.config["ALLOWED_OUTPUT_IMAGE_EXTENSIONS"]:
-            return True
+        return True
     else:
         return False
 
-@app.route('/uploadInput', methods=["GET","POST"])
+
+@app.route('/uploadInput', methods=["GET", "POST"])
 def uploadInput():
     if request.method == "POST":
         if request.files:
             folder = request.files.getlist("input-folder-2[]")
             timestamped = datetime.now().strftime('%Y-%m-%d_%H%M%S')
-            folderpath = os.path.join(app.config["IMAGE_UPLOADS"], "ecSegOutput", timestamped, "orig")
+            folderpath = os.path.join(
+                app.config["IMAGE_UPLOADS"], "ecSegOutput", timestamped, "orig")
             os.makedirs(folderpath)
             for file in folder:
                 print(file.filename)
@@ -58,7 +59,7 @@ def uploadInput():
                     path = os.path.join(
                         app.config["IMAGE_UPLOADS"], "ecSegOutput", timestamped, file.filename)
                     file.save(path)
-                    print("Image saved" +path)
+                    print("Image saved" + path)
                 else:
                     print("not allowed")
     # RUN ECSEG HERE
@@ -70,7 +71,8 @@ def uploadInput():
     # return render_template('visualize.html'+'/'+str(imagelist[0]))
     return redirect(request.url)
 
-@app.route('/uploadecSeg', methods=["GET","POST"])
+
+@app.route('/uploadecSeg', methods=["GET", "POST"])
 def uploadecSeg():
     if request.method == "POST":
         if request.files:
@@ -95,8 +97,9 @@ def uploadecSeg():
                     print(file.filename+" saved")
                 else:
                     print("not allowed")
-    path = os.path.join(app.config["IMAGE_UPLOADS"], "ecSegOutput", timestamped, "orig")+'/'
-    session['folder']=timestamped
+    path = os.path.join(app.config["IMAGE_UPLOADS"],
+                        "ecSegOutput", timestamped, "orig")+'/'
+    session['folder'] = timestamped
     session['imagelist'] = imglist(path)
     session['imagename'] = session['imagelist'][0]
     return redirect('/visualize')
@@ -107,6 +110,7 @@ def newimgselect(img):
     session['imagename'] = img
     return redirect('/visualize')
 
+
 @app.route('/visualize')
 def visualize():
     return render_template('visualize.html', images=session['imagelist'], folder=session['folder'], imgname=session['imagename'])
@@ -116,5 +120,5 @@ def visualize():
 def mpDetector():
     return render_template('mpDetector.html')
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+
