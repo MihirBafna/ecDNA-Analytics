1033131594feb521731054efcc26f8ecf6174938
Mihir Bafna <mihir.bafna@gmail.com>
2020-06-12 16:15:43 -0700

    added basic styles for input page

--- app/static/js/input.js
+++ app/static/js/input.js
@@ -3,7 +3,6 @@ $("#disabled").click(function () {
 });
 $(".custom-file-input").on("change", function () {
     var fileName = $(this).val().split("\\").pop();
-    alert(fileName);
     $(this).siblings(".custom-file-label").addClass("selected").html("Folder Uploaded");
     $(".selected").css("color", "rgb(0,0,0,0.3)");
 });
