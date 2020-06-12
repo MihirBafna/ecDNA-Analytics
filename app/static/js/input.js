d29bb4d990ccb36c6d3e33da7d443a12a0a3c33e
Mihir Bafna <mihir.bafna@gmail.com>
2020-06-12 14:47:29 -0700

    added info buttons

--- app/static/js/input.js
+++ app/static/js/input.js
@@ -3,6 +3,27 @@ $("#disabled").click(function () {
 });
 $(".custom-file-input").on("change", function () {
     var fileName = $(this).val().split("\\").pop();
+    alert(fileName);
     $(this).siblings(".custom-file-label").addClass("selected").html("Folder Uploaded");
     $(".selected").css("color", "rgb(0,0,0,0.3)");
+});
+$('#firstinfo').popover({
+    container: "body",
+    html: true,
+    title: function () {
+        return $("#firstinfo").html();
+    },
+    content: function () {
+        return '<div class="popover-message">' + $(this).data("message") + '</div>';
+    }
+});
+$('#secondinfo').popover({
+    container: "body",
+    html: true,
+    title: function () {
+        return $("#secondinfo").html();
+    },
+    content: function () {
+        return '<div class="popover-message">' + $(this).data("message") + '</div>';
+    }
 });
