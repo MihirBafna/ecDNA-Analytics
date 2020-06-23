105db2f2de8351972637564fb759af962c250c96
Mihir Bafna <mihir.bafna@gmail.com>
2020-06-23 14:57:51 -0700

    added directvisualize route

--- app/routes.py
+++ app/routes.py
@@ -5,6 +5,7 @@ from flask import render_template, redirect, request, session, send_from_directo
 import os
 from werkzeug.utils import secure_filename
 from datetime import datetime
+from flask.helpers import flash
 
 
 
@@ -39,16 +40,18 @@ def uploadInput():
                     print("Image saved " + path)
                 else:
                     print("not allowed")
-    # RUN ECSEG HERE
-    tools.runecSeg(folderpath,1)
-    im.reorganizeOutput(timestamped)
-    path = os.path.join(app.config["IMAGE_UPLOADS"],
-                        "ecSegOutput", timestamped, "orig")+'/'
-    session['folder'] = timestamped
-    im.tiffToPNG(timestamped)
-    session['imagelist'] = im.imglist(path)
-    session['imagename'] = session['imagelist'][0]
-    return redirect('/visualize')
+        # RUN ECSEG HERE
+        tools.runecSeg(folderpath,1)
+        im.reorganizeOutput(timestamped)
+        path = os.path.join(app.config["IMAGE_UPLOADS"],
+                            "ecSegOutput", timestamped, "orig")+'/'
+        session['folder'] = timestamped
+        im.tiffToPNG(timestamped)
+        session['imagelist'] = im.imglist(path)
+        session['imagename'] = session['imagelist'][0]
+        return redirect('/visualize')
+    else:
+        return render_template('input.html')
 
 
 
@@ -76,14 +79,16 @@ def uploadecSeg():
                     print(file.filename+" saved")
                 else:
                     print(file.filename+" not allowed")
-    # im.reorganizeOutput(timestamped)
-    path = os.path.join(app.config["IMAGE_UPLOADS"],
-                        "ecSegOutput", timestamped, "orig")+'/'
-    session['folder'] = timestamped
-    im.tiffToPNG(timestamped)
-    session['imagelist'] = im.imglist(path)
-    session['imagename'] = session['imagelist'][0]
-    return redirect('/visualize')
+        # im.reorganizeOutput(timestamped)
+        path = os.path.join(app.config["IMAGE_UPLOADS"],
+                            "ecSegOutput", timestamped, "orig")+'/'
+        session['folder'] = timestamped
+        im.tiffToPNG(timestamped)
+        session['imagelist'] = im.imglist(path)
+        session['imagename'] = session['imagelist'][0]
+        return redirect('/visualize')
+    else:
+        return render_template('input.html')
 
 
 @app.route('/visualize/<img>')
@@ -98,15 +103,23 @@ def visualize():
     return render_template('visualize.html', images=session['imagelist'], folder=session['folder'], imgname=session['imagename'])
 
 
-@app.route('/directvisualize/<folder>')
-def directVisualize(folder):
-    path = os.path.join(app.config["IMAGE_UPLOADS"],
-                        "ecSegOutput", folder, "orig")+'/'
-    session['folder'] = folder
-    session['imagelist'] = im.imglist(path)
-    session['imagename'] = session['imagelist'][0]
-    return redirect('/visualize')
-
+@app.route('/directvisualize', methods=["GET", "POST"])
+def directVisualize():
+    if request.method=="POST":
+        folder = request.form['folder']
+        print(folder)
+        path = os.path.join(app.config["IMAGE_UPLOADS"],"ecSegOutput", folder, "orig")+'/'
+        if os.path.exists(path):
+            flash(f'Folder {folder} has been selected.', 'success')
+            session['folder'] = folder
+            session['imagelist'] = im.imglist(path)
+            session['imagename'] = session['imagelist'][0]
+            return redirect('/visualize')
+        else:
+            flash(f'Invalid folder. Folder name {folder} not recognized.', 'danger')
+            return render_template('input.html')
+    else:
+        return render_template('input.html')
 
 @app.route('/downloadIMG/<img>/<folder>')
 def downloadIMG(img,folder):   
