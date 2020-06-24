80fc7226ef8dabae4b0bbc6f92feeaf5beec3399
Mihir Bafna <mihir.bafna@gmail.com>
2020-06-24 13:44:55 -0700

    added function to check for proper output folder structure

--- app/imagemanipulation.py
+++ app/imagemanipulation.py
@@ -53,6 +53,36 @@ def imglist(filepath):  # get image data after ecSeg is run
             imagelist.append(file)
     return(imagelist)
 
+def correctOutputFolderStructure(path):
+    directory = os.listdir(path)
+    orig = False
+    dapi = False
+    labels = False
+    onum = 0
+    dnum = 0
+    lnum = 0
+    def countPNG(folder):
+        count=0
+        for file in os.listdir(os.path.join(path, folder)):
+            if file.endswith(".png"):
+                count+=1
+        return count
+    for folder in directory:
+        if folder=='orig':
+            onum = countPNG(folder)
+            orig = True
+        if folder == 'dapi':
+            dnum = countPNG(folder)
+            dapi = True
+        if folder == 'labels':
+            lnum = countPNG(folder)
+            labels = True
+    return orig and dapi and labels and onum == dnum == lnum != 0
+
+def correctInputFolderStructure(path):
+    return
+
+
 def compressImg(name, path, folder):
         # zipper = zipfile.ZipFile(name[:-4]+'.zip' , "w")
     # # zipper.write(os.path.join(path))
