ebb4307f5eaae9584fa40ed3c9bb8125027a0881
Mihir Bafna <mihir.bafna@gmail.com>
2020-06-28 14:17:22 -0700

    added up button directing back to homepage

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,10 +1,17 @@
 $(document).ready(function () {
+    $("#up").hide();
     $("#start").click(function () {
-        // console.log($("#toolslide").offset().top);
         $('html, body').animate({
             scrollTop: $("#toolslide").offset().top
         }, 2000
         );
-        $(".footer").animate({ background: 'rgba(37, 193, 255, 0.6)' }, 2000);
+        $("#up").show(3000);
+    });
+    $("#up").click(function () {
+        $('html, body').animate({
+            scrollTop: -$("#toolslide").offset().top
+        }, 2000
+        );
+        $("#up").hide(3000);
     });
 });
